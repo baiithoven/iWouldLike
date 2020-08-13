14bb09c1f3fb0f84b55ef0c396ec50a1bddca591
baithoven <julienlucas16@gmail.com>
2020-08-13 14:43:55 +0200

    Added CRUD

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,15 +5,11 @@ namespace App\Controller;
 use App\Entity\Game;
 use App\Repository\GameRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Repository\RepositoryFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class GameController extends AbstractController
@@ -22,22 +18,29 @@ class GameController extends AbstractController
      * @Route("/game", name="game", methods={"POST"})
      *
      * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @param SerializerInterface $serializer
      * @return JsonResponse
      */
-    public function index(EntityManagerInterface $entityManager)
+    public function index(EntityManagerInterface $entityManager, Request $request, SerializerInterface $serializer)
     {
-        $game =  (new Game())
-            ->setName('Minecraft')
-            ->setCategory('Survival')
-            ->setImage('https://i1.pngguru.com/preview/964/49/199/minecraft-icon-minecraft-minecraft-png-clipart.jpg')
-            ->setDeveloppers('Mojang')
-            ->setPlatform('None')
-            ->setPublisher('Microsoft')
-        ;
 
+        /** @var Game $game */
+        $game = $serializer->deserialize($request->getContent(), Game::class, 'json');
         $entityManager->persist($game);
         $entityManager->flush();
-        return new JsonResponse(['message' => 'Saved new product with id '.$game->getId()]);
+        return new JsonResponse(['message' => 'Saved new game with id ' . $game->getId()]);
+//        $game = (new Game())
+//            ->setName('Minecraft')
+//            ->setCategory('Survival')
+//            ->setImage('https://i1.pngguru.com/preview/964/49/199/minecraft-icon-minecraft-minecraft-png-clipart.jpg')
+//            ->setDeveloppers('Mojang')
+//            ->setPlatform('None')
+//            ->setPublisher('Microsoft');
+//
+//        $entityManager->persist($game);
+//        $entityManager->flush();
+//        return new JsonResponse(['message' => 'Saved new product with id ' . $game->getId()]);
     }
 
     /**
@@ -51,16 +54,86 @@ class GameController extends AbstractController
     public function fetch(int $id, GameRepository $gameRepository, SerializerInterface $serializer)
     {
         $game = $gameRepository->find($id);
-
-        if(!$game)
-        {
+        if (!$game) {
             throw $this->createNotFoundException(
-                'No game found for id' .$id
+                'No game found for id' . $id
             );
         }
-        $jsonContent = $serializer->serialize($game,'json');
+        $jsonContent = $serializer->serialize($game, 'json');
         return new Response($jsonContent, 200, [
             "Content-Type" => 'application/json'
         ]);
+    }
+
+    /**
+     * @Route ("/game", name="get_games",methods={"GET"})
+     * @param GameRepository $gameRepository
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function fetchAll(GameRepository $gameRepository, SerializerInterface $serializer)
+    {
+        $games = $gameRepository->findAll();
+        if (!$games) {
+            throw $this->createNotFoundException(
+                'No games found'
+            );
+        }
+        $jsonContent = $serializer->serialize($games, 'json');
+        return new Response($jsonContent, 200, [
+            "Content-Type" => 'application/json' //show that the response is in JSON
+        ]);
+    }
+
+    /**
+     * @Route ("/game/{id}", name="delete_game", methods={"DELETE"})
+     * @param EntityManagerInterface $entityManager
+     * @param int $id
+     * @param GameRepository $gameRepository
+     * @return Response
+     */
+    public function delete(EntityManagerInterface $entityManager, int $id, GameRepository $gameRepository): Response
+    {
+        $game = $gameRepository->find($id);
+        if (!$game) {
+            throw $this->createNotFoundException(
+                'No game found'
+            );
+        }
+        $entityManager->remove($game);
+        $entityManager->flush();
+        return new JsonResponse(['message' => 'Deleted game with id ' . $id]);
+    }
+
+    /**
+     * @Route ("/game/{id}", name="edit_game", methods={"PUT"})
+     * @param EntityManagerInterface $entityManager
+     * @param int $id
+     * @param GameRepository $gameRepository
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function edit(EntityManagerInterface $entityManager, int $id, GameRepository $gameRepository, Request $request, SerializerInterface $serializer): Response
+    {
+        $game = $gameRepository->find($id);
+
+        if (!$game) {
+            throw $this->createNotFoundException(
+                'No game found'
+            );
+        }
+        /** @var Game $gameEdit */
+        $gameEdit = $serializer->deserialize($request->getContent(), Game::class, 'json');
+        $game->setName($gameEdit->getName())
+            ->setPublisher($gameEdit->getPublisher())
+            ->setCategory($gameEdit->getCategory())
+            ->setDeveloppers($gameEdit->getDeveloppers())
+            ->setPlatform($gameEdit->getPlatform())
+            ->setImage($gameEdit->getImage())
+        ;
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Edited game with id ' . $id]);
     }
 }
